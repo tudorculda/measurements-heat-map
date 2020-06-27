82b558196427f755e9a021d7f12b361f7888d902
Tudor <tudor.culda@endava.com>
2020-06-27 17:01:01 +0300

    keep only useful

--- main.js
+++ main.js
@@ -1,105 +1,108 @@
-function getCenterOutBoundCircle(p1, p2, p3){
-    const y12 = p1.y - p2.y;
-    const x12 = p1.x - p2.x;
-    const y32 = p3.y - p3.y; 
-    const x32 = p3.x - p2.x;
-    const d21 = p2.y * p2.y - p1.y * p1.y + p2.x * p2.x - p1.x * p1.x;
-    const d23 = p2.y * p2.y - p3.y * p3.y + p2.x * p2.x - p3.x * p3.x;
-    y0 = ( ( d21 * x32 / x12 ) - d23 ) / ( 2 * ( y32 - ( y12 * x32 / x12)));
-    x0 = ( ( d21 * y32 / y12 ) - d23 ) / ( 2 * ( x32 - ( x12 * y32 / y12)));
-    return {x:x0, y:y0}
-};
-
-
-function drawTrinagleEchiR(ctx, r){
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, r);
-    ctx.lineTo(r/2 * Math.sqrt(3), r/2);
-    ctx.closePath();
+function putCross( ctx, p){
+    for ([i,point] of p.entries()){
+        ctx.beginPath()
+        ctx.moveTo(point.x-5, point.y-5);
+        ctx.lineTo(point.x+5, point.y+5);
+        ctx.moveTo(point.x-5, point.y+5);
+        ctx.lineTo(point.x+5, point.y-5);
+        ctx.stroke();
+        ctx.fillText(i,point.x+7,point.y+7);
+    }
 }
 
-
-function drawTriangle(ctx, p1, p2, p3){
-    let xa = p1.x;
-    let ya = p1.y;
-    let xb = p2.x;
-    let yb = p2.y;
-    let xc = p3.x;
-    let yc = p3.y;
-    let r = 1000;
-    let RAD3 = Math.sqrt(3);
-    let a,b,c,d,e,f;
-    e = xa * 1.0;
-    f = ya * 1.0;
-    c = ( xb  - e ) * 1.0 / r;
-    d = ( yb - f ) * 1.0 / r;
-    a = ( xc - e - c*r/2 ) * 2.0 / ( r * RAD3 );
-    b = ( yc - f - d*r/2 ) * 2.0 / ( r * RAD3 );
-    
-    ctx.save();
-    
-    ctx.setTransform(a, b, c, d, e, f); 
-    
-    var grd = ctx.createRadialGradient(0,0,10, 0,0, r*RAD3/2);
-    // grd.addColorStop(0, p1.c + 'FF');
-    // grd.addColorStop(1, p1.c + '00');
-    grd.addColorStop(0, p1.c );
-    grd.addColorStop(1, "#000000");
-    // Fill with gradient
-    drawTrinagleEchiR(ctx, r);
-    ctx.fillStyle = grd;
-    ctx.fill();
-    
-    ctx.globalCompositeOperation = 'lighter';
-    
-    // Create gradient
-    grd = ctx.createRadialGradient(0,r,10, 0, r, r*RAD3/2);
-    // grd.addColorStop(0, p2.c + 'FF');
-    // grd.addColorStop(1, p2.c + '00');
-    grd.addColorStop(0, p2.c);
-    grd.addColorStop(1, "#000000");
-    
-    drawTrinagleEchiR(ctx, r);
-    // Fill with gradient
-    ctx.fillStyle = grd;
-    ctx.fill();
-    
-    // Create gradient
-    grd = ctx.createRadialGradient(RAD3 *r/2, r/2 ,10, RAD3 *r/2, r/2 , r*RAD3/2);
-    // grd.addColorStop(0, p3.c + 'FF');
-    // grd.addColorStop(1, p3.c + '00');
-    grd.addColorStop(0, p3.c);
-    grd.addColorStop(1, "#000000");
-       drawTrinagleEchiR(ctx, r);
-    // Fill with gradient
-    ctx.fillStyle = grd;
-    ctx.fill();
-    
-    ctx.restore()
+function getProjectionDistance(a, b, c){
+    const k2 = b.x*b.x - b.x*a.x + b.y*b.y -b.y*a.y;
+    const k1 = a.x*a.x - b.x*a.x + a.y*a.y -b.y*a.y;
+    const ab2 = (a.x - b.x)*(a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+    const kcom = (c.x*(a.x - b.x) + c.y*(a.y-b.y));
+    const d1 = (k1 - kcom) / ab2;
+    const d2 = (k2 + kcom) / ab2;
+    return {d1, d2};
 }
 
+function limit01(value){
+    if(value < 0){
+        return 0;
+    }
+    if(value > 1){
+        return 1;
+    }
+    return value;
+}
+function paddingleft0(v, v_length){
+    while( v.length < v_length){
+        v = '0' + v;
+    }
+    return v;
+}
+
+function getWeightedColorMix(points, ratios){
+    let r = 0;
+    let g = 0;
+    let b = 0;
+    for( [ind, point] of points.entries()){
+        r += Math.round(parseInt(point.c.substring(1,3), 16) * ratios[ind]);
+        g += Math.round(parseInt(point.c.substring(3,5), 16) * ratios[ind]);
+        b += Math.round(parseInt(point.c.substring(5,7), 16) * ratios[ind]);
+    }
+        
+    let result = '#' + paddingleft0(r.toString(16),2) + paddingleft0(g.toString(16),2) + paddingleft0(b.toString(16),2);
+
+    return result;
+}
+
+/**
+ * Given some points with color attached, calculate the color for a new point
+ * @param  p The new point position {x: number, y: number}
+ * @param  points The array of given colored points [{x: nember, y: number, c: hexColor}]
+ * @return hex color string -- The weighted color mix
+ */
+function getGeometricColorMix( p, points ){
+    let colorRatios = new Array(points.length);
+    colorRatios.fill(1);
+    for ( [ind1, point1] of points.entries()){
+        for ( [ind2, point2] of points.entries()){
+            if( ind1 != ind2){
+                d  = getProjectionDistance(point1, point2, p);
+                colorRatios[ind1] *= limit01(d.d2);
+            }
+        }
+
+    }
+      let totalRatiosSum = 0;
+      colorRatios.forEach(c => totalRatiosSum += c);
+      colorRatios.forEach((c,i) => colorRatios[i] /= totalRatiosSum);
+    c = getWeightedColorMix(points, colorRatios);
+    return c;
+}
+
+let points = [
+    {x:10, y:10, c:"#FF0000"},
+    {x:70, y:150, c:"#FFFF00"},
+    {x:224, y:300, c:"#00FF00"},
+    {x:121, y:100, c:"#00FFFF"},
+    {x:160, y:10, c:"#FF00FF"},
+]; // these are the starting points for drawing the gradient
 
 
 var canv = document.getElementById("myCanvas");
 var ctx = canv.getContext("2d");
-p1 = {x:0, y:100, c:'#FF0000'};
-p2 = {x:50, y:600, c:'#00FF00'};
-p3 = {x:150, y:200, c:'#202020'};
-p4 = {x:400, y:400, c:'#FFFFFF'};
+putCross(ctx, points); // optional, only to show the original point position
+ctx.globalCompositeOperation = 'destination-over'; // to show the cross points over the gradient
+let xcs = points.map( p => p.x);
+let ycs = points.map( p => p.y);
+let xmin = Math.min(...xcs);
+let xmax = Math.max(...xcs);
+let ymin = Math.min(...ycs);
+let ymax = Math.max(...ycs);
+let x, y;
+let mixColor;
 
-p5 = {x:30, y:20, c:'#008080'}
-drawTriangle(ctx, p1, p2, p3);
-// drawTriangle(ctx, p2, p3, p4);
-// drawTriangle(ctx, p1, p3, p5);
-// drawTriangle(ctx, p5, p3, p4);
-// let c534 = getCenterOutBoundCircle(p5,p3,p4);
-// ctx.beginPath();
-// ctx.moveTo(c534.x, c534.y);
-// ctx.lineTo(p4.x, p4.y);
-// ctx.moveTo(c534.x, c534.y);
-// ctx.lineTo(p5.x, p5.y);
-// ctx.moveTo(c534.x, c534.y);
-// ctx.lineTo(p3.x, p3.y);
-// ctx.stroke();
-
+// iterate all the pixels between the given points
+for( x = xmin; x < xmax; x++ ){
+    for( y = ymin; y < ymax; y++ ){
+        mixColor = getGeometricColorMix({x:x, y:y}, points);
+        ctx.fillStyle = mixColor;
+        ctx.fillRect(x, y, 1, 1);
+    }
+}
